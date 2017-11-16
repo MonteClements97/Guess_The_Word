919dc905980bee1f8c1df184e48795346f85f7a9
Montell Clements <moc17@aber.ac.uk>
2017-11-16 14:50:07 +0000

    Changed the word to be guessed from list to string

--- guess_the_word.py
+++ guess_the_word.py
@@ -2,6 +2,6 @@ import start_up
 import play_guess_the_word
 start_up.get_settings()
 input("A small project by Monte Clements")
-play_guess_the_word.play_guess_the_word(['H', 'e', 'l', 'l', 'o'], 8)
+play_guess_the_word.play_guess_the_word("Hello", 8)
 
 
